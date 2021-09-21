44c262d4185f4755b4c80713ca4c95a6a3b8109d
MarcinDa <91115825+MarcinDa@users.noreply.github.com>
2021-09-21 09:55:08 +0200

    Add files via upload

---
+++ 21.09.21/script.js
@@ -0,0 +1,6 @@
+document.write("Javascript<br>");
+document.write("\"Koperek\"<br>");
+document.write(-.4e9+ "<br>");
+document.write(-0xCD+"<br>");
+var a = /*parseInt*/(prompt("Podaj liczbe bo będzie źle: "));
+document.write(a+"  aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
